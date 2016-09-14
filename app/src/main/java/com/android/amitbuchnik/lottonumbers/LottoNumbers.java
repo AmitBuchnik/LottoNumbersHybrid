a2988799db12f2f9785b2bcdf4dc4002d809eb04
AmitBuchnik <amitbuhnik@gmail.com>
2016-09-14 15:24:13 +0300

    Added Action bar

--- app/src/main/java/com/android/amitbuchnik/lottonumbers/LottoNumbers.java
+++ app/src/main/java/com/android/amitbuchnik/lottonumbers/LottoNumbers.java
@@ -1,14 +1,10 @@
 package com.android.amitbuchnik.lottonumbers;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.pm.ApplicationInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
-import android.view.Display;
-import android.view.WindowManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -22,6 +18,10 @@ public class LottoNumbers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lotto_numbers);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setBackgroundResource(R.drawable.action_bar_bg);
+        setSupportActionBar(myToolbar);
+
         WebView.setWebContentsDebuggingEnabled(true);
         browser = (WebView) findViewById(R.id.webView);
         browser.setWebViewClient(new URLIntercepter());
@@ -30,6 +30,29 @@ public class LottoNumbers extends AppCompatActivity {
         webSettings.setDomStorageEnabled(true); // allow local storage
         webSettings.setAllowUniversalAccessFromFileURLs(true); //allow ajax from file url
         browser.loadUrl("file:///android_asset/www/lottonumbers.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            browser.loadUrl("file:///android_asset/www/lottonumbers.html#settings");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
